526a571676b62b8bdae7d217666e15ab615d7619
Ashleigh Chute <ashlhc@uw.edu>
2023-06-26 16:35:46 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/App.css';
 import App from './App';
-import { HashRouter as Router } from "react-router-dom";
 import { getCLS, getFCP, getFID, getLCP, getTTFB } from 'web-vitals';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 getCLS(console.log);
 getFCP(console.log);
 getFID(console.log);
